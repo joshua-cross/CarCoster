c5f33d1807e9a7ef7dcb2a00e0374b9d73177aca
Josh Cross <crossjoshua6@gmail.com>
2017-12-12 14:07:37 +0000

    Each unique manufactorer now only shows up once rather than multiple times.

--- CarCoster/Form1.cs
+++ CarCoster/Form1.cs
@@ -12,6 +12,8 @@ namespace CarCoster
 {
     public partial class Form1 : Form
     {
+        List<Car> cars = new List<Car>();
+
         public Form1()
         {
             InitializeComponent();
@@ -24,16 +26,57 @@ namespace CarCoster
             string fileLoc = thisDirectory + @"\Images\LeafIcon.png";
 
             JsonReader json = new JsonReader();
-            List<Car> cars = json.getCars();
-            foreach (Car car in cars)
-            {
-                CarBox.Items.Add(car.Manufacturer);
-                
-            }
+            drawListBox(json);
+
+
 
             pictureBox1.Image = Image.FromFile(fileLoc);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
             Title.Text = fileLoc;
+        }
+
+        private void drawListBox(JsonReader json)
+        {
+
+            /*List of string that will hold each of the unique manufactorers.*/
+            List<string> manufactorers = new List<string>();
+            List<Car> cars = json.getCars();
+            try
+            {
+                foreach (Car car in cars)
+                {
+                    //the manufactorer.
+                    string theManufactorer = car.Manufacturer;
+                    //boolean that checks if the current manufactorer is unique or not.
+                    bool isNew = true;
+                    /*Only add the manufactorer to the list if 
+                     it's not already */
+                     foreach (string manufactorer in manufactorers)
+                    {
+                        if(car.Manufacturer.Equals(manufactorer))
+                        {
+                            //setting is new to false at it already exists in the array.
+                            isNew = false;
+                            break;
+                        }
+                    }
+
+                     //if the manufactorer is new then add to the array and the list.
+                     if(isNew)
+                    {
+                        manufactorers.Add(car.Manufacturer);
+                        CarBox.Items.Add(car.Manufacturer);
+
+                    }
+
+                }
+            }
+            catch (Exception error)
+            {
+
+                CarBox.Items.Add(error.ToString());
+
+            }
         }
     }
 }
