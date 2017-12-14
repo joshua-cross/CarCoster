3e3a06ee86bc1175d5990e89c1ad507c90080619
Josh Cross <crossjoshua6@gmail.com>
2017-12-14 14:44:24 +0000

    Changed the car printer class so now it only prints what is apploicable to the car rather than everything, e.g. if a diesel car it will not print the electricity cost.

--- CarCoster/Car.cs
+++ CarCoster/Car.cs
@@ -13,27 +13,27 @@ namespace CarCoster
         public string Transmission { get; set; }
         public int? EngineCapacity { get; set; }
         public string FuelType { get; set; }
-        public float ElecricEnergyConsumptionMiles { get; set; }
-        public float MilesPerKilowatt { get; set; }
-        public float MaximumRangeKm { get; set; }
-        public float MaximumRangeMiles { get; set; }
-        public float MetricUrbanCold { get; set; }
-        public float MetricExtraUrban { get; set; }
-        public float MetricCombined { get; set; }
-        public float ImperialUrbanCold { get; set; }
-        public float ImperialExtraUrban { get; set; }
-        public float ImperialCombined { get; set; }
-        public float CO2gramsPerKilometer { get; set; }
-        public float FuelCostPer12000Miles { get; set; }
-        public float ElectricityCost { get; set; }
-        public float CostPer12000Miles { get; set; }
-        public int EuroStandard { get; set; }
-        public float NoiseLevelDB { get; set; }
-        public float EmissionCO2 { get; set; }
-        public float THCLevel { get; set; }
-        public float NOxEmissions { get; set; }
-        public float THCAndNOxEmissions { get; set; }
-        public float Particulates { get; set; }
+        public float? ElecricEnergyConsumptionMiles { get; set; }
+        public float? MilesPerKilowatt { get; set; }
+        public float? MaximumRangeKm { get; set; }
+        public float? MaximumRangeMiles { get; set; }
+        public float? MetricUrbanCold { get; set; }
+        public float? MetricExtraUrban { get; set; }
+        public float? MetricCombined { get; set; }
+        public float? ImperialUrbanCold { get; set; }
+        public float? ImperialExtraUrban { get; set; }
+        public float? ImperialCombined { get; set; }
+        public float? CO2gramsPerKilometer { get; set; }
+        public float? FuelCostPer12000Miles { get; set; }
+        public float? ElectricityCost { get; set; }
+        public float? CostPer12000Miles { get; set; }
+        public int? EuroStandard { get; set; }
+        public float? NoiseLevelDB { get; set; }
+        public float? EmissionCO2 { get; set; }
+        public float? THCLevel { get; set; }
+        public float? NOxEmissions { get; set; }
+        public float? THCAndNOxEmissions { get; set; }
+        public float? Particulates { get; set; }
         public string blank { get; set; }
     }
 }
--- CarCoster/CarPrinter.cs
+++ CarCoster/CarPrinter.cs
@@ -12,25 +12,125 @@ namespace CarCoster
         {
             string sCar = "Manufactorer: " + car.Manufacturer + "\n" +
                           "Model: " + car.Model + "\n" +
-                          "Description: " + car.Description + "\n" + 
-                          "Transmission: " + car.Transmission + "\n" +
-                          "Engine Capacity: " + car.EngineCapacity + "\n" +
-                          "Fuel Type: " + car.FuelType + "\n" +
-                          "Fuel Consumption Metric Urban (Cold): " + car.MetricUrbanCold + "\n" +
-                          "Fuel Consumption Metric Extra-Urban: " + car.MetricExtraUrban + "\n" + 
-                          "Fuel Consumption Metric Combined: " + car.MetricCombined + "\n" +
-                          "Fuel Consumption Imperial Urban (Cold): " + car.ImperialUrbanCold + "\n" +
-                          "Fuel Consumption Imperial Extra-Urban: " + car.ImperialUrbanCold + "\n" +
-                          "Fuel Consumption Imperial Combined: " + car.ImperialCombined + "\n" +
-                          "CO2 g/km: " + car.CO2gramsPerKilometer + "\n" + 
-                          "Fuel Cost per 12000 miles: £" + car.FuelCostPer12000Miles + "\n" +
-                          "Euro Standard: " + car.EuroStandard + "\n" +
-                          "Noise Level dB(A): " + car.NoiseLevelDB + "\n" +
-                          "Carbon Monoxide (CO) Emissions: " + car.EmissionCO2 + "\n" +
-                          "THC Emissions (mg/km): " + car.THCLevel + "\n" +
-                          "Nitrogen Oxide (NOx) emissions (mg/km): " + car.NOxEmissions + "\n" +
-                          "THC + NOx Emissions (mg/km): " + car.THCAndNOxEmissions + "\n" +
-                          "Particulates (mg/km)" + car.Particulates + "\n";
+                          "Description: " + car.Description + "\n";
+                          
+            if(car.Transmission != null)
+            {
+                sCar += "Transmission: " + car.Transmission + "\n";
+            }
+
+            if(car.EngineCapacity != null)
+            {
+                sCar += "Engine Capacity: " + car.EngineCapacity + "\n";
+            }
+
+            if(car.FuelType != null)
+            {
+                sCar += "Fuel Type: " + car.FuelType + "\n";
+
+            }
+
+            if(car.ElecricEnergyConsumptionMiles != null)
+            {
+                sCar += "Electric Energy Consumptions Miles/kWh: " + car.ElecricEnergyConsumptionMiles + "\n";
+            }
+
+            if(car.MilesPerKilowatt != null)
+            {
+                sCar += "wh/km: " + car.MilesPerKilowatt + "\n";
+            }
+
+            if (car.MaximumRangeKm != null)
+            {
+                sCar += "Maximum Range (Km): " + car.MaximumRangeKm + "\n";
+            }
+
+            if(car.MaximumRangeMiles != null)
+            {
+                sCar += "Maximum Range (Miles): " + car.MaximumRangeMiles + "\n";
+            }
+            if (car.MetricUrbanCold != null)
+            {
+                sCar += "Fuel Consumption Metric Urban (Cold): " + car.MetricUrbanCold + "\n";
+
+            }
+
+            if(car.MetricExtraUrban != null)
+            {
+                sCar += "Fuel Consumption Metric Extra-Urban: " + car.MetricExtraUrban + "\n";
+            }
+
+            if (car.MetricCombined != null)
+            {
+                sCar += "Fuel Consumption Metric Combined: " + car.MetricCombined + "\n";
+            }
+
+            if (car.ImperialUrbanCold != null)
+            {
+                sCar += "Fuel Consumption Imperial Urban (Cold): " + car.ImperialUrbanCold + "\n";
+            }
+            
+            if(car.ImperialExtraUrban != null)
+            {
+                sCar += "Fuel Consumption Imperial Extra-Urban: " + car.ImperialUrbanCold + "\n";
+
+            }
+
+            if(car.ImperialCombined != null)
+            {
+                sCar += "Fuel Consumption Imperial Combined: " + car.ImperialCombined + "\n";
+            }
+
+            if(car.CO2gramsPerKilometer != null)
+            {
+                sCar += "CO2 g/km: " + car.CO2gramsPerKilometer + "\n";
+            }
+
+            if(car.FuelCostPer12000Miles != null)
+            {
+                sCar += "Fuel Cost per 12000 miles: £" + car.FuelCostPer12000Miles + "\n";
+            }
+
+            if(car.ElectricityCost != null)
+            {
+                sCar += "Electricity Cost per 12000 miles: £" + car.ElectricityCost + "\n";
+            }
+            if(car.EuroStandard != null)
+            {
+                sCar += "Euro Standard: " + car.EuroStandard + "\n";
+            }
+
+            if(car.NoiseLevelDB != null)
+            {
+                sCar += "Noise Level dB(A): " + car.NoiseLevelDB + "\n";
+
+            }
+
+            if(car.EmissionCO2 != null)
+            {
+                sCar += "Carbon Monoxide (CO) Emissions: " + car.EmissionCO2 + "\n";
+
+            }
+
+            if(car.THCLevel != null)
+            {
+                sCar += "THC Emissions (mg/km): " + car.THCLevel + "\n";
+            }
+
+            if(car.NOxEmissions != null)
+            {
+                sCar += "Nitrogen Oxide (NOx) emissions (mg/km): " + car.NOxEmissions + "\n";
+            }
+
+            if(car.THCAndNOxEmissions != null)
+            {
+                sCar += "NOx and THC Emissions (mg/km): " + car.THCAndNOxEmissions + "\n";
+            }
+
+            if(car.Particulates != null)
+            {
+                sCar += "Particulates (mg/km)" + car.Particulates + "\n";
+            }
 
             return sCar;
         }
