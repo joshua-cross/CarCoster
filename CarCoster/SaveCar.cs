5ec69365cd36287389161eb70b0e8042427ca33f
Josh Cross <crossjoshua6@gmail.com>
2017-12-15 14:39:41 +0000

    Added an if statement in savecar that checks if we have over 5 cars, also added a button in the overview that takes you to the addcar form

--- CarCoster/SaveCar.cs
+++ CarCoster/SaveCar.cs
@@ -19,6 +19,35 @@ namespace CarCoster
             {
                 try
                 {
+                    //we cannot own more than 5 cars.
+                    if(Properties.Settings.Default.Manufacturers.Count > 5
+                       && Properties.Settings.Default.Models.Count > 5
+                       && Properties.Settings.Default.Descriptions.Count > 5)
+                    {
+
+                        //the first element in the array (which we want removing.)
+                        int oldIndex = 0;
+
+                        Properties.Settings.Default.Manufacturers.RemoveAt(oldIndex);
+                        Properties.Settings.Default.Models.RemoveAt(oldIndex);
+                        Properties.Settings.Default.Descriptions.RemoveAt(oldIndex);
+
+                        for(int i = 1; i < Properties.Settings.Default.Manufacturers.Count; i = i + 1)
+                        {
+
+                            string man = Properties.Settings.Default.Manufacturers[i];
+                            string mod = Properties.Settings.Default.Models[i];
+                            string desc = Properties.Settings.Default.Descriptions[i];
+
+                            Properties.Settings.Default.Manufacturers.Remove(man);
+                            Properties.Settings.Default.Manufacturers.Insert(i - 1, man);
+                            Properties.Settings.Default.Models.Remove(mod);
+                            Properties.Settings.Default.Models.Insert(i - 1, mod);
+                            Properties.Settings.Default.Descriptions.Remove(desc);
+                            Properties.Settings.Default.Descriptions.Insert(i - 1, desc);
+                        }
+
+                    }
                     Properties.Settings.Default.Manufacturers.Add(car.Manufacturer);
                     Properties.Settings.Default.Models.Add(car.Model);
                     Properties.Settings.Default.Descriptions.Add(car.Description);
